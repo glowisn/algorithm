1ea92f028a8659bd6ad302a806fe899831f19dd7
Yechan Jung <asdxcv1998@gmail.com>
2022-07-29 13:37:26 +0900

    Create 10869.cpp

---
+++ 10869.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl << a - b << endl << a * b << endl << a / b << endl << a % b << endl;
+    return 0;
+}
