e9cca7e38a6ecbfa9ae19ce0f6261c957a03d7f3
Yechan Jung <asdxcv1998@gmail.com>
2022-12-21 13:16:01 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a,b;
+  cin >> a;
+  cin >> b;
+  cout << a * (b % 10) << endl;
+  cout << a * (b/10 % 10) << endl;
+  cout << a * (b/100 % 10) << endl;
+  cout << a * b << endl;
+} 
