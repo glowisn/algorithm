66a5fbb6df46024bf11f50fec738365ae8171006
Yechan Jung <asdxcv1998@gmail.com>
2023-01-05 17:19:53 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 80 ms, Memory: 31104 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
@@ -1,4 +1,6 @@
 #9148
+from sys import stdin
+input = stdin.readline
 
 table = {}
 
@@ -8,32 +10,32 @@ def hash(a,b,c):
         return table[(0,0,0)]
     if (a,b,c) in table:
         return table[(a,b,c)]
-    else:
-        return -99
+    elif (a,b,c) not in table:
+        return None
 
 def calculate(a,b,c):
     global table
-    if hash(a,b,c) != -99:
+    if hash(a,b,c) != None:
         return hash(a,b,c)
 
     if a < b and b < c:
-        if hash(a,b,c-1) == -99:
+        if hash(a,b,c-1) == None:
             calculate(a,b,c-1)
-        if hash(a,b-1,c-1) == -99:
+        if hash(a,b-1,c-1) == None:
             calculate(a,b-1,c-1)
-        if hash(a,b-1,c) == -99:
+        if hash(a,b-1,c) == None:
             calculate(a,b-1,c)
 
         table[(a,b,c)] = hash(a, b, c-1) + hash(a, b-1, c-1) - hash(a, b-1, c)
 
     else:
-        if hash(a-1,b,c) == -99:
+        if hash(a-1,b,c) == None:
             calculate(a-1,b,c)
-        if hash(a-1,b-1,c) == -99:
+        if hash(a-1,b-1,c) == None:
             calculate(a-1,b-1,c)
-        if hash(a-1,b,c-1) == -99:
+        if hash(a-1,b,c-1) == None:
             calculate(a-1,b,c-1)
-        if hash(a-1,b-1,c-1):
+        if hash(a-1,b-1,c-1) == None:
             calculate(a-1,b-1,c-1)
 
         table[(a,b,c)] = hash(a-1, b, c) + hash(a-1, b-1, c) + hash(a-1, b, c-1) - hash(a-1, b-1, c-1)
