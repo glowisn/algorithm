e600c5069643b6b3b4e2f21df8dea55b7ac4d580
Yechan Jung <asdxcv1998@gmail.com>
2023-01-10 16:14:32 +0900

    [Silver III] Title: 수리공 항승, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1449. 수리공 항승/수리공 항승.py
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.py
@@ -1,15 +1,12 @@
-#1449
-
+#1449_re
 N, L = map(int,input().split())
-
 holes = list(map(int,input().split()))
-
+holes.sort()
 tapes = []
 tapecnt = 0
-for d in range(1,1001):
-    if d in holes and d not in tapes:
+for h in holes:
+    if h not in tapes:
         tapecnt += 1
         for i in range(L):
-            tapes.append(d+i)
-
+            tapes.append(h+i)
 print(tapecnt)
