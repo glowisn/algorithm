4f550d38b60faef9a7b0276b64bde1e917e5338d
Yechan Jung <asdxcv1998@gmail.com>
2023-01-12 17:54:31 +0900

    [Silver II] Title: 우리집엔 도서관이 있어, Time: 148 ms, Memory: 44556 KB -BaekjoonHub

--- 백준/Silver/2872. 우리집엔 도서관이 있어/우리집엔 도서관이 있어.py
+++ 백준/Silver/2872. 우리집엔 도서관이 있어/우리집엔 도서관이 있어.py
@@ -6,10 +6,7 @@ n = int(input())
 books = [int(input()) for _ in range(n)]
 
 maxbook = n
-cnt = 0
 for book in books[::-1]:
     if book == maxbook:
-        cnt += 1
         maxbook -= 1
-
-print(n - cnt)
+print(maxbook)
