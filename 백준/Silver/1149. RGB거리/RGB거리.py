6ea8c7e008ef81978cbaa74a422dc1867805f486
Yechan Jung <asdxcv1998@gmail.com>
2023-01-16 17:02:56 +0900

    [Silver I] Title: RGB거리, Time: 72 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,11 +1,9 @@
 #1149
 n = int(input())
+cost = list(map(int,input().split()))
 
-costs = [list(map(int,input().split())) for _ in range(n)]
-summs = [costs[0]]
+for i in range(n-1):
+    r,g,b = map(int,input().split())
+    cost = [r+min(cost[1],cost[2]),g+min(cost[0],cost[2]),b+min(cost[0],cost[1])]
 
-
-for i in range(1,n):
-    summs.append([costs[i][0] +  min(summs[i-1][1] , summs[i-1][2]), costs[i][1] +  min(summs[i-1][0] , summs[i-1][2]),costs[i][2] +   min(summs[i-1][0] , summs[i-1][1])])
-
-print(min(summs[i]))
+print(min(cost))
