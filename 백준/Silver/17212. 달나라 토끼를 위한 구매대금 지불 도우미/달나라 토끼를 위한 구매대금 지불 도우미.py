8feff7d33576438fb5691aaf8866e0aa6390ae9f
Yechan Jung <asdxcv1998@gmail.com>
2023-01-30 13:36:52 +0900

    [Silver III] Title: 달나라 토끼를 위한 구매대금 지불 도우미, Time: 56 ms, Memory: 34328 KB -BaekjoonHub

---
+++ 백준/Silver/17212. 달나라 토끼를 위한 구매대금 지불 도우미/달나라 토끼를 위한 구매대금 지불 도우미.py
@@ -0,0 +1,15 @@
+#17212
+n = int(input())
+dp = [100000 for _ in range(n+1)]
+dp[:8] = [0,1,1,2,2,1,2,1]
+
+if n % 7 == 3:
+    cnt = 2
+    for i in range(10,n+1,7):
+        dp[i] = cnt
+        cnt += 1
+else:
+    for i in range(8,n+1):
+        dp[i] = dp[i-7] + 1
+
+print(dp[n])
