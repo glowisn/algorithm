82aeaf966e121dbe64350ef56476f01c3643ffd5
Yechan Jung <asdxcv1998@gmail.com>
2023-02-03 15:56:42 +0900

    [Gold V] Title: LCS, Time: 536 ms, Memory: 55712 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,12 @@
+#9251
+a,b = input(),input()
+dp = [[0] * (len(b)+1) for _ in range(len(a)+1)]
+
+for i in range(1,len(a)+1):
+    for j in range(1,len(b)+1):
+        if a[i-1] == b[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i-1][j] , dp[i][j-1])
+
+print(dp[-1][-1])
