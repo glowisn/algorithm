fe49ea936b97caceeb90d965a05a8012ccb7e8c4
Yechan Jung <asdxcv1998@gmail.com>
2023-02-10 16:34:51 +0900

    [Silver III] Title: 문자열 집합, Time: 3752 ms, Memory: 36124 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -0,0 +1,14 @@
+#14425
+from sys import stdin
+input = stdin.readline
+
+N,M = map(int,input().rsplit())
+S = [input().rstrip() for _ in range(N)]
+
+ans = 0
+for _ in range(M):
+    t = input().rstrip()
+    if t in S:
+        ans += 1
+    
+print(ans)
