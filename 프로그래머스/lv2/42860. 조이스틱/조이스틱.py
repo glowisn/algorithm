05f182605f3016c3408cc4f6fe427070acf823f5
Yechan Jung <asdxcv1998@gmail.com>
2023-06-03 18:32:14 +0900

    [level 2] Title: 조이스틱, Time: 0.05 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/lv2/42860. 조이스틱/조이스틱.py
@@ -1,12 +1,45 @@
 def getlr(name:str):
-    min_move = len(name) - 1
-    for i, c in enumerate(name):
-        next_i = i+1
-        while next_i < len(name) and name[next_i] == 'A':
-            next_i += 1
-        # 각 문자부터 'A..'문자가 있을경우 몇번씩 조이스틱쓰는지 체크
-        min_move = min(min_move, 2*i+ len(name)-next_i, 2*(len(name)-next_i)+i)
-    return min_move
+    max_value = len(name) - 1
+    tname = list(name)
+    tname[0] = 'A'
+    lcount = 0
+    ind = len(tname)-1
+    while any(al!='A' for al in tname):
+        tname[ind] = 'A'
+        ind -= 1
+        lcount += 1
+    tname = list(name)
+    tname[0] = 'A'
+    rcount = 0
+    ind = 0
+    while any(al!='A' for al in tname):
+        ind += 1
+        tname[ind] = 'A'
+        rcount += 1
+    print(lcount,rcount)
+    minlen = min(lcount,rcount)
+        
+    # get super 'A's
+    maxx, t, maxlind = 0,0,0
+    for i in range(len(name)):
+        if i == 0:
+            continue
+        if name[i] == 'A':
+            t += 1
+        else:
+            if maxx < t:
+                maxlind = i
+                maxx = t
+            t = 0
+    if maxx < t:
+        maxlind = i+1
+        maxx = t
+    # super A is name[maxlind-maxx:maxlind]
+    left = len(name) - maxlind
+    right = max(maxlind-maxx - 1,0)
+    # print(right*2+left,left*2+right,right,left)
+    # print(max_value,minlen,left*2+right,right*2+left)
+    return min(max_value,minlen,left*2+right,right*2+left)
 
 def getud(name:str):
     s = 0
@@ -19,5 +52,5 @@ def solution(name):
         return 0
     a = getud(name)
     b = getlr(name)
-    print(a,b)
+    # print(a,b)
     return a + b
